bc792d64d178e32388ee2655b579f6c9b2c3724d
tjwogns <tjwogns0820@gmail.com>
2021-07-27 20:12:48 +0900

    [*] Add Room and Database class

---
+++ data/src/main/java/com/tjwogns/data/db/BreedDao.kt
@@ -0,0 +1,7 @@
+package com.tjwogns.data.db
+
+import androidx.room.Dao
+
+@Dao
+interface BreedDao {
+}
--- presentation/src/main/java/com/tjwogns/presentation/paging2/Paging2ListViewModel.kt
+++ presentation/src/main/java/com/tjwogns/presentation/paging2/Paging2ListViewModel.kt
@@ -3,6 +3,7 @@ package com.tjwogns.presentation.paging2
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagedList
 import com.tjwogns.domain.model.BreedSnippet
 import com.tjwogns.domain.usecase.BreedUseCase
 import com.tjwogns.presentation.base.BaseViewModel
@@ -14,16 +15,16 @@ class Paging2ListViewModel @Inject constructor(
     private val breedUseCase: BreedUseCase
 ): BaseViewModel() {
 
-    private val _breeds = MutableLiveData<List<BreedSnippet>>()
-    val breeds: LiveData<List<BreedSnippet>> = _breeds
+    private val _breeds = MutableLiveData<PagedList<BreedSnippet>>()
+    val breeds: LiveData<PagedList<BreedSnippet>> = _breeds
 
     init {
 //        getBreeds()
     }
 
     private fun getBreeds() {
-        breedUseCase(viewModelScope) { result ->
-            _breeds.value = result.map { BreedSnippet(it) }
-        }
+//        breedUseCase(viewModelScope) { result ->
+//            _breeds.value = result.map { BreedSnippet(it) }
+//        }
     }
 }
