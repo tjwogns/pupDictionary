3791763144f4be0bd50f3bc6814af9a787b23ac1
tjwogns <tjwogns0820@gmail.com>
2021-07-31 15:53:22 +0900

    database dependency injecting

--- data/src/main/java/com/tjwogns/data/db/PupDatabase.kt
+++ data/src/main/java/com/tjwogns/data/db/PupDatabase.kt
@@ -1,34 +1,10 @@
 package com.tjwogns.data.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
-import com.tjwogns.data.model.BreedDto
+import com.tjwogns.data.entity.BreedEntity
 
-@Database(entities = [BreedDto::class], version = 1)
-abstract class PupDatabase(): RoomDatabase() {
+@Database(entities = [BreedEntity::class], version = 1)
+abstract class PupDatabase: RoomDatabase() {
     abstract fun breedDao() : BreedDao
-
-    companion object {
-        // For Singleton instantiation
-        @Volatile
-        private var instance: PupDatabase? = null
-
-        fun getInstance(context: Context): PupDatabase {
-            return instance ?: synchronized(this) {
-                instance ?: buildDatabase(context).also { instance = it }
-            }
-        }
-
-        private fun buildDatabase(context: Context): PupDatabase {
-            return Room.databaseBuilder(context, PupDatabase::class.java, "pup_db")
-                .addCallback(object : RoomDatabase.Callback() {
-                    override fun onCreate(db: SupportSQLiteDatabase) {
-                        super.onCreate(db)
-                    }
-                }).build()
-        }
-    }
 }
